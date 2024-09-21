1bb8aa48a7953c26ae7f60d88b192b00ec5af12c
pawan nagar <nagarpawan065@gmail.com>
2024-09-21 08:22:56 +0530

    initial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("pawan");
+console.log(23)
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let pawan_matilda = 'JM'
+let $function = 27;
+
+let person = 'pawan'
+let PI = 3.1414;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31
+
+const birthYear = 1991;
+//birthYear = 1991;
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
